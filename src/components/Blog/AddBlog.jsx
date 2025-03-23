bd6987097064601260e5583edc32cbe948fb0735
Mahmuda-14 <mahmudaiiuc14@gmail.com>
2025-03-23 13:54:01 +0600

    done

--- src/components/Blog/AddBlog.jsx
+++ src/components/Blog/AddBlog.jsx
@@ -56,7 +56,7 @@ const AddBlog = () => {
     <div className="p-16">
       <h2 className="text-4xl text-black text-center p-10">Add Blog</h2>
       <div className="flex flex-row">
-        <div className="bg-violet-200 w-2/3 max-h-[65vh] rounded-xl p-8">
+        <div className="bg-violet-200 w-2/3 max-h-[70vh] rounded-xl p-8">
           <div className="mb-8 flex">
             <div className="w-1/2">
               <label className="label">

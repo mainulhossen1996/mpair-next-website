30745fe393b3d280f4e68aabc57d37ed03e63fce
NTNirjana <nafijatabassum79@gmail.com>
2025-04-21 15:33:17 +0600

    Career Page Done

---
+++ src/app/career/[id]/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import JobDetails from "@/components/Career/JobDetails";
+
+const page = () => {
+ 
+
+  return (
+    <div>
+     
+        <JobDetails  />
+     
+    </div>
+  );
+};
+
+export default page;
